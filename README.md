a29c773b9b475471959a90b4d13cc0612e3e68e4
Angelo Verlain <geoangercola@gmail.com>
2022-04-23 00:26:26 +0200

    re-add documentation & add JSDoc to source

---
+++ README.md
@@ -0,0 +1,322 @@
+### written
+
+[![Build Status](https://travis-ci.org/stephenhutchings/written.svg)](https://travis-ci.org/stephenhutchings/written)
+
+_written_ provides a set of utilities for manipulating text, with a focus on
+providing typographic tools rather than pure string manipulation. It can be
+added as a set of mixins to Underscore or used in it's own right, both in front
+and back end contexts.
+
+- [Capitalization](#capitalization)
+- [Utilities](#utilities)
+- [Collapse](#collapse)
+- [Cases](#cases)
+- [Tags](#tags)
+- [Lists](#lists)
+- [Hyphenation](#hyphenation)
+- [Quantify](#quantify)
+- [Written Numbers](#written-numbers)
+- [Quotes](#quotes)
+- [Ordinals](#ordinals)
+- [Numbers](#numbers)
+- [Glyphs](#glyphs)
+- [Language Support](#language-support)
+
+##### Deno or ESM
+
+```js
+import * as written from "https://deno.land/x/written/written.ts";
+
+// or import singular functions
+import { writtenNumber } from "https://deno.land/x/written/written.ts";
+```
+
+##### Browser
+
+```html
+<script type="module" src=></script>
+```
+
+##### NPM
+
+As this is a Deno fork, the original NPM version is published by
+@stephenhutchings from the repo
+[stephenhutchings/written](https://www.github.com/stephenhutchings/written).
+Note that the Node version doesn't have types.
+
+```bash
+npm install written
+```
+
+---
+
+#### Setup
+
+_written_ can be used in any environment that supports both ES Modules and
+TypeScript.
+
+Some style guides prefer the numbers 12 and under to be written, so we'll
+include those in here. If more or fewer numbers need to be added, or those from
+another language, see [Language Support](#language-support).
+
+Following the APA style guide (for ease and practicality) conjunctions,
+articles, and short prepositions of less than four letters will be left in
+lowercase when calling `capitalizeAll()`.
+
+A rule is needed to determine the correct ordinal for any number. For English,
+we use match in such a way that the first value in the matching array is
+returned, unless it is 11, 12 or 13. We use this number to determine the correct
+ordinal form.
+
+---
+
+#### Capitalization
+
+Capitalize the first letter of a string.
+
+Examples:
+
+```ts
+w.capitalize("obviously");                       // Obviously
+```
+
+Capitalize all words in a string apart from some common lower case words. This
+can be tested with the internal noncaps regular expression, which are stored by
+language code, or by passing a regular expression of your own.
+
+Examples:
+
+```ts
+w.capitalizeAll("this and that");                // This and That
+w.capitalizeAll("the cat in the hat");           // The Cat in the Hat
+```
+
+#### Utilities
+
+`enclose` wraps a string within two other strings, repeating the first if needs
+be. `cleanJoin` joins an array of words with falsy, non-string values removed
+with some glue. Both are used internally but are offered in case of their
+external value.
+
+Examples:
+
+```ts
+w.enclose("'", "string");                        // 'string'
+w.cleanJoin(["this", null, "that"], " and ");    // this and that
+```
+
+#### Collapse
+
+Replace all white-space in a string with a single space character
+
+Examples:
+
+```ts
+w.collapse("this   \t\t and \n    that");        // this and that
+```
+
+#### Cases
+
+Transform strings between common code cases.
+
+Examples:
+
+```ts
+w.camelCase("some-thing");                       // someThing
+w.hyphenCase("some_thing");                      // some-thing
+w.snakeCase("someThing");                        // some_thing
+w.humanCase("fromA_to-Z");                       // from A to Z
+```
+
+This helps to split "cased" words into their constituent parts...
+
+#### Tags
+
+Enclose a string inside an HTML tag.
+
+Examples:
+
+```ts
+w.wrapInTag("Hello world!");                     // <span>Hello world!</span>
+w.wrapInTag("Hello world!", "em");               // <em>Hello world!</em>
+w.wrapInTag(                                     // <a href="/url" class="b" disabled="disabled">Link</a>
+  "Link",
+  "a",
+  {
+    href: "/url",
+    class: ["b"],
+    disabled: true,
+  },
+);
+```
+
+#### Lists
+
+Group strings into a grammatically correct list with an arbitrary limit. The
+final example shows all the possible options available.
+
+Examples:
+
+```ts
+w.prettyList(["Ben", "Bob"])                     // Ben and Bob
+w.prettyList(["Ben", "Bob", "Bill"])             // Ben, Bob and Bill
+w.prettyList(["Ben", "Bob", "Bill", "Max"], 2)   // Ben, Bob and 2 more
+w.prettyList(["Ben", "Bob"], 1, {more: "other"}) // Ben and 1 other
+w.prettyList([                                   // Document 1 & two other files
+  {file: "Document 1"},
+  {file: "Document 2"},
+  {file: "Document 3"}
+], 1, {
+  amp: "&"
+  written: true,
+  more: "other file",
+  quantify: true,
+  key: "file"
+})
+```
+
+#### Hyphenation
+
+Add soft hyphens every `n` characters so that the CSS attribute
+`hyphens: manual` will allow for nice breaks in long strings of text. This is
+especially useful on mobile devices, where long strings can break the layout.
+
+Examples:
+
+```ts
+w.hyphenate("antidisestablishmentarianism"); // antidisest%C2%ADablishm...
+```
+
+#### Quantify
+
+Add an "s" to a string when an amount is non-singular, disregarding the order of
+the arguments passsed. If an array or collection is passed, it’s length will be
+used as the numerical input.
+
+Examples:
+
+```ts
+w.quantify("monkey", 1);                         // 1 monkey
+w.quantify(1, "monkey");                         // 1 monkey
+w.quantify("monkey", 9, { written: true });      // nine monkeys
+w.quantify("person", 9, { plural: "people" });   // 9 people
+w.quantify([1, 2, 3], "number");                 // 3 numbers
+```
+
+#### Written Numbers
+
+Convert numbers between one and twelve into their written counter-parts.
+
+Examples:
+
+```ts
+w.writtenNumber(1);                              // one
+w.writtenNumber(2, "DE");                        // zwei
+```
+
+#### Quotes
+
+Wrap a string in single or double quotes or guillemets (angle quotes).
+
+Examples:
+
+```ts
+w.quote("pastry chef", "s");                     // ‘pastry chef’
+w.quote("cats cradle");                          // “cats cradle”
+w.quote("tres chic", "a");                       // «tres chic»
+w.quote("Gol", "!");                             // ¡Gol!
+w.quote("Cómo estás", "?");                      // ¿Cómo estás?
+```
+
+#### Ordinals
+
+Convert a number from it's cardinal to ordinal equivalent.
+
+Examples:
+
+```ts
+w.ordinal(1);                                    // 1st
+w.ordinal(2, { written: true });                 // second
+w.ordinal(3, { wrap: true });                    // 3<sup>rd</sup>
+w.ordinal(4, { wrap: "em" });                    // 4<em>th</em>
+```
+
+#### Numbers
+
+Format a number in various ways and parse one from a string.
+
+Examples:
+
+```ts
+w.prettyNumber(1000);                            // 1,000
+w.prettyNumber(10.5, 2);                         // 10.50
+w.prettyNumber(9999, " ", 2, ",");               // 9 999,00
+
+w.prettyPrice(4);                                // $4.00
+w.prettyPrice(1200, "£");                        // £1,200.00
+w.prettyPrice(                                   // €4<sup>00</sup>
+  4,
+  {
+    currency: "€",
+    wrap: "sup",
+  },
+);
+
+w.prettyPercent(0.5);                            // 50%
+w.prettyPercent(1, 4);                           // 25%
+w.prettyPercent(1, 3, 2);                        // 33.33%
+
+w.parseNumber(1000);                             // 1000
+w.parseNumber("1,000.00");                       // 1000
+w.parseNumber("99%");                            // 0.99
+w.parseNumber("some 44,000 participants");       // 44000
+```
+
+#### Glyphs
+
+Provide quick access to different typographic glyphs without the need commit
+them to memory or look at a reference table.
+
+Examples:
+
+```ts
+w.glyphs();                                      // Create map of ASCII glyphs
+w.glyph("!");                                    // &#33;
+```
+
+#### Language Support
+
+Set cardinal and ordinal numbers and non-caps words for different languages as
+appropriate. Please note that only partial support for French, German, Italian,
+Spanish and Swedish is currently implemented. If using in the browser, ensure
+that the document's charset is set to UTF-8. _Pull requests which extend
+language support are encouraged._
+
+To load and install a locale, do:
+
+```ts
+import * as written from "https://deno.land/x/written/written.ts";
+import * as DE from "https://deno.land/x/written/lang/written.de.ts";
+// there is also es,fs,it and se.
+
+written.setLanguage(DE.dico, DE.code);
+
+// then use it like so
+w.writtenNumber(2, "DE");                        // zwei
+```
+
+#### Aliases
+
+Pack up the `written` object (with some aliases...)
+
+```json
+{
+  "count": quantify
+  "dasherize": hyphenCase
+  "dashify": hyphenCase
+  "numerate": quantify
+  "slugify": snakeCase
+  "titleCase": capitalizeAll
+  "underscore": snakeCase
+}
+```
--- written.ts
+++ written.ts
@@ -1,15 +1,29 @@
 /**
  * #### Rules
  */
+
+/** A number can either be a string or a map of genders and the corresponding string */
 type Written = string | Record<string, string>;
+
+/**
+ * A language is described in the object
+ * None of the properties are required and will be gracefully replaced by the english counterparts
+ */
 export interface Language {
+  /** A regex that matches words which should not be capitalised */
   noncaps: RegExp;
+  /** Cardinal letters */
   cardinals: {
+    /** An array of ordinal letters */
     written: Written[];
   };
+  /** Ordinal letters */
   ordinals: {
+    /** An array of ordinal letters */
     written: Written[];
+    /** A regex that matches numbers that can be converted to ordinals */
     rule: RegExp;
+    /** suffixes to ordinals (such as (1)st) */
     suffixes: Record<string, Written>;
   };
 }
@@ -66,8 +80,10 @@ const dictionary: Record<string, Language> = {
 
 /**
  * Capitalize the first letter of a string
- * @param str
- * @returns
+ * @example
+ * w.capitalize("obviously");                    // Obviously
+ * @param str the string to capitalize
+ * @returns the capitalized string
  */
 export const capitalize = (str: string) =>
   str.charAt(0).toUpperCase() + str.slice(1);
@@ -76,6 +92,12 @@ export const capitalize = (str: string) =>
  * Capitalize all words in a string apart from some common lower case words.
  * This can be tested with the internal noncaps regular expression, which are
  * stored by language code, or by passing a regular expression of your own.
+ * @example
+ * w.capitalizeAll("this and that");             // This And That
+ * @param str The string to capitalize
+ * @param init Regex that matches words which should not be capitalised or a
+ * language code
+ * @returns 
  */
 export const capitalizeAll = (
   str: string,
@@ -99,65 +121,84 @@ export const capitalizeAll = (
     .join(" ");
 };
 
-/**
- * #### Utilities
- */
+// Utilities
 
+/**
+ * Wraps a string within two other strings, repeating the first if it needs be
+ * @param a The string to wrap with
+ * @param b The string to wrap
+ * @param c The string to wrap with at the end (if provided)
+ * @returns wrapped string
+ */
 export const enclose = (a: string, b: string, c?: string) =>
   `${a}${b}${c || a}`;
 
+/**
+ * Joins an array of words with falsy, non-string values removed with some glue.
+ * @param arr The array of words to join
+ * @param glue The glue to join the words with
+ * @returns The joined string
+ */
 export const cleanJoin = function (arr: Array<string | null>, glue = "") {
   return arr
     .filter((string) => (string && typeof string === "string"))
     .join(glue);
 };
 
+/**
+ * Replace all white-space in a string with a single space character
+ * @param str The string to remove whitespace from
+ * @returns The string with whitespace removed
+ */
 export const collapse = (str: string) => str.replace(/\s+/g, " ");
+
+// cases
 
 const caseSplitter = /[-_\s]+|(!?[A-Z][a-z]*)/g;
 
+/**
+ * Converts a string to camel case (pascal case)
+ * @param str The string to convert
+ * @returns The string in camel case
+ */
 export const camelCase = (str: string) => {
-  // cleanJoin(
-  //   str
-  //     .split(caseSplitter)
-  //     .filter((part) => !!part)
-  //     .map((part, index) => {
-  //       if (!part) return;
-  //       if (index === 0) {
-  //         return part;
-  //       } else {
-  //         return capitalize(part);
-  //       }
-  //     }),
-  // );
   return str.replace(/[\s_-]+(\w)/g, (_a, w) => w.toUpperCase());
 };
 
+/**
+ * Converts a string to hyphen case (with hyphens/dashes between words)
+ * @param str The string to convert
+ * @param leading Whether to add a hyphen as the first letter
+ * @returns The string in hyphen case
+ */
 export const hyphenCase = (str: string, leading?: boolean) =>
   (leading ? "-" : "") +
   cleanJoin(str.split(caseSplitter), "-").toLowerCase();
 
+/**
+ * Converts a string to snake case (with underscores)
+ * @param str The string to convert
+ * @returns The string in snake case
+ */
 export const snakeCase = (str: string) =>
   cleanJoin(str.split(caseSplitter), "_").toLowerCase();
 
+/**
+ * Converts a string to human case (separated by white space)
+ * @param str The string to convert
+ * @returns The string in human case
+ */
 export const humanCase = (str: string) =>
   cleanJoin(str.split(caseSplitter), " ");
 
 /**
- * @example
- * wrapInTag(
-  "Link",
-  "a",
-  {
-    href: "/url",
-    class: ["b"],
-    disabled: true
-  }
-)
- * @param str
- * @param tag
- * @param attributes
- * @returns
+ * Enclose a string inside an HTML tag.
+ * @param str The string to wrap
+ * @param tag The tag to wrap the string with
+ * @param attributes The attributes to add to the tag
+ * attributes is an object whose values can either be strings, booleans or an
+ * array of strings eg. `{ class: ["foo", "bar"], id: "baz", hidden: true }`
+ * @returns string wrapped in tag with attributes
  */
 export const wrapInTag = (
   str: string,
@@ -174,19 +215,32 @@ export const wrapInTag = (
   return enclose(`<${tag}${attrs.join("")}>`, str, `</${tag}>`);
 };
 
-/**
- * Lists
- */
+// Lists
+
 interface PrettyListOptions {
+  /** The string to use for "more" */
   more?: string;
+  /** The string used to join items (ampersand) */
   amp?: string;
+  /** The accessor used to access values when the provided value is an array */
   key?: string;
+  /** Which tag to use to wrap the items with */
   wrap?: string;
+  /** Whether to pluralize the items */
   quantify?: boolean;
+  /** Whether to write the remaining items in words */
   written?: boolean;
+  /** The language to use */
   lang?: string;
 }
 
+/**
+ * Pretty-print a list of items.
+ * @param arr The array of items to pretty print
+ * @param max The maximum number of items to show
+ * @param opts The options to use
+ * @returns The pretty printed list
+ */
 export const prettyList = (
   arr: unknown[],
   max?: number,
@@ -228,10 +282,17 @@ export const prettyList = (
     .concat(arr.length === 1 ? "" : ` ${amp} `, ...arr.slice(-1) as string[]);
 };
 
-/**
- * Hyphenation
- */
+// hyphenation
 
+/**
+ * Add soft hyphens every `n` characters so that the CSS attribute
+ * `hyphens: manual` will allow for nice breaks in long strings of text. This is
+ * especially useful on mobile devices, where long strings can break the layout.
+ * @param str The string to hyphenate
+ * @param n The number of characters after which a hyphen is added
+ * @param softHyphen Which hyphen to use (default is soft hyphen)
+ * @returns The hyphenated string
+ */
 export const hyphenate = (str: string, n = 10, softHyphen = "\u00AD"): string =>
   str
     .replace(/(^|[^>])+(?=$|\<)/g, (sub) =>
@@ -240,16 +301,30 @@ export const hyphenate = (str: string, n = 10, softHyphen = "\u00AD"): string =>
         (_w, a, b) => a + softHyphen + b,
       ));
 
-/**
- * Quantify
- */
+// quantify
 
 interface QuantifyOptions {
+  /** Whether to display the number in front of the word (default true) */
   numberless?: boolean;
+  /** Whether to write the number as a word instead of digits (default false) */
   written?: boolean;
+  /** The language to use */
   lang?: "EN";
+  /** The plural of the word to use (instead of adding an `s`) */
   plural?: string;
 }
+
+/**
+ * Add an "s" to a string when an amount is non-singular, disregarding the order of
+ * the arguments passsed. If an array or collection is passed, it’s length will be
+ * used as the numerical input.
+ * @param first The number of items or an array of items (the length of which
+ * is used)
+ * @param second The name of items
+ * **NOTE:** The first arguement can be interchanged with the second one
+ * @param options The options to use
+ * @returns A string depicting the items and their number
+ */
 export const quantify = (
   first: string | number | Array<unknown>,
   second: string | number | Array<unknown>,
@@ -285,8 +360,14 @@ export const quantify = (
   return strn + s;
 };
 
+// Written numbers
+
 /**
- * Written numbers
+ * Convert numbers between one and twelve into their written counter-parts.
+ * @param n The number
+ * @param lang The language to use
+ * @param gender The required gender of the word
+ * @returns written counterpart of a number
  */
 export const writtenNumber = (n: number, lang = "EN", gender = "m"): string => {
   const num = dictionary[lang]?.cardinals.written[n - 1];
@@ -303,9 +384,7 @@ export const writtenNumber = (n: number, lang = "EN", gender = "m"): string => {
   }
 };
 
-/**
- * Quotes
- */
+// quotes
 type Quotes =
   | "s"
   | "single"
@@ -317,6 +396,13 @@ type Quotes =
   | "?"
   | "q"
   | string;
+
+/**
+ * Wrap a string in single or double quotes or guillemets (angle quotes).
+ * @param str The string to quote
+ * @param type The type of quotes to use
+ * @returns The quoted string
+ */
 export const quote = (str: string, type?: Quotes) => {
   let a: string, z: string;
   switch (type) {
@@ -343,14 +429,24 @@ export const quote = (str: string, type?: Quotes) => {
   return enclose(a, str, z);
 };
 
-/**
- * Ordinals
- */
+// ordinals
+
 interface OrdinalOptions {
+  /** Whether to use the written form of the number (default false) */
   written?: boolean;
+  /** Wrap the suffix in a tag (when true, tag is `sup`) (default false) */
   wrap?: boolean | string;
+  /** The language to use */
   lang?: "EN";
 }
+
+/**
+ * Convert a number from it's cardinal to ordinal equivalent.
+ * @param n The number
+ * @param opts The options to use
+ * @param gender The required gender of the written number
+ * @returns The ordinal form of a cardinal number
+ */
 export const ordinal = (
   n: number,
   opts: OrdinalOptions = {},
@@ -381,8 +477,16 @@ export const ordinal = (
   }
 };
 
+// numbers
+
 /**
- * Numbers
+ * Pretty print a number
+ * @param n The number
+ * @param delimiter The delimiter to use after every 3 digits (thousand) or the
+ * number of digits after the decimal points
+ * @param decimals The number of digits after the decimal point
+ * @param dot The decimal point to use
+ * @returns The pretty printed number
  */
 export const prettyNumber = (
   n: number,
@@ -405,11 +509,17 @@ export const prettyNumber = (
 };
 
 interface Currency {
+  /** The symbok of the currency */
   currency?: string;
+  /** The tag to wrap the currency in */
   wrap?: string;
+  /** The number of digits after the decimal point */
   decimals?: number;
+  /** The delimiter to use after every 3 digits (thousand) */
   delimiter?: string;
+  /** The decimal point to use */
   dot?: string;
+  /** Whether to show the currency sign at the beginning of number (default true) */
   front?: boolean;
 }
 export const prettyPrice = (n: number, currency?: string | Currency) => {
@@ -441,11 +551,23 @@ export const prettyPrice = (n: number, currency?: string | Currency) => {
   }
 };
 
+/**
+ * Pretty print a percentage
+ * @param numerator The numerator of the percentage / The value
+ * @param denominator The denominator of the percentage / The max value
+ * @param decimals The number of digits after the decimal point (default 0)
+ * @returns The pretty printed percentage
+ */
 export const prettyPercent = (numerator = 0, denominator = 1, decimals = 0) => {
   const percent = (numerator / denominator * 100) || 0;
   return `${percent.toFixed(decimals)}%`;
 };
 
+/**
+ * Extract a number from a string (or number)
+ * @param n The string/number to extract the number from
+ * @returns The extracted number
+ */
 export const parseNumber = (n: number | string) => {
   if (typeof n === "string") {
     n = parseFloat(n.replace(/[^\d\.]+/g, "")) /
@@ -458,15 +580,19 @@ export const parseNumber = (n: number | string) => {
   }
 };
 
-/**
- * Glyphs
- */
+// glyphs
+
 const fromTo = (x: number, y: number) =>
   new Array(y - x + 1).fill(0)
     // Array
     //   .apply(0, { length: y - x + 1 })
     .map((_e, i) => i + x);
 
+/**
+ * Provide quick access to different typographic glyphs without the need commit
+ * them to memory or look at a reference table.
+ * @returns a map of glyphs
+ */
 export const glyphs = (): Record<string, string> =>
   Object.fromEntries(
     [
@@ -479,11 +605,23 @@ export const glyphs = (): Record<string, string> =>
       .map((code) => [code, String.fromCharCode(code)]),
   );
 
+/**
+ * Get the code point of a certain character
+ * @param c the character
+ * @returns the code point of the character eg: &#33;
+ */
 export const glyph = (c: string) =>
   enclose("&#", c.charCodeAt(0).toString(), ";");
 
+// language support
+
 /**
- * Language support
+ * Set cardinal and ordinal numbers and non-caps words for different languages as
+appropriate. Please note that only partial support for French, German, Italian,
+Spanish and Swedish is currently implemented.
+ * @param object The language dictionary
+ * @param lang The language code used to reference this language dictionary
+ * @returns The language dictionary
  */
 export const setLanguage = (object: Partial<Language>, lang: string) =>
   dictionary[lang] = { ...dictionary.EN, ...object };
